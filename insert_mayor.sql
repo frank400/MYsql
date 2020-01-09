8d2788585a7056c2b503c6b8b44570d4556bd9a2
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-09 02:21:33 -0300

    insert mayor and not mayorsto the mayor table

---
+++ insert_mayor.sql
@@ -0,0 +1,9 @@
+insert into `prefeitos` (nome,cidade_id)
+values 
+    ('Jonas Donizette',1),
+    ('Rodrigo Neves Barreto',2);
+
+insert into `prefeitos` (nome,cidade_id)
+values
+    ('not_a_mayor',null),
+    ('lawyer',null);
