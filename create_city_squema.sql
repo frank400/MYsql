54002fad5b69e8b1b8d1df718595b8178c0a9576
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-09 00:01:52 -0300

    new city table

---
+++ create_city_squema.sql
@@ -0,0 +1,8 @@
+create table if not exists cidade(
+    id int unsigned not null auto_increment,
+    nome varchar(255) not null,
+    estado_id int unsigned not null,
+    area decimal(10,2),
+    PRIMARY key (id),
+    FOREIGN KEY (estado_id) references states (id)
+);
