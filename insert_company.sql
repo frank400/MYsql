c50d57ef78320bcd1bc9a22b02a84d11114e10fd
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-09 03:43:45 -0300

    query that insert company

--- insert_company.sql
+++ insert_company.sql
@@ -1,3 +1,10 @@
+insert into company
+    (nome,cnpj)
+values
+    ('bradesco',34234523),
+    ('vale',5623452),
+    ('inter',242353123);
+
 insert into company_units
     (company_id,cidade_id,sede)
 values
